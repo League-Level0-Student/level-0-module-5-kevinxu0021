12b483c03e1efac92e6b2975bbfda5161cd025c4
league <league@wts20.attlocal.net>
2018-03-31 12:57:48 -0700

    Fibonacci, Prime Or Not, and Goofy Names

---
+++ src/Fibonacci.java
@@ -0,0 +1,56 @@
+
+public class Fibonacci {
+public static void main(String[] args) {
+	
+	int first = 0;
+	int second = 1;
+	System.out.println(first);
+	System.out.println(second);
+	for (int i = 0; i < 45; i++) {
+		int temp = first + second;
+		first = second;
+		second = temp;
+		System.out.println(temp);
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
